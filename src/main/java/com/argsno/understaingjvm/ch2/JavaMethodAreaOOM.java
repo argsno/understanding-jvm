56751a957bc391363b84d471ac2bccb75e64631e
argsno <argsno@gmail.com>
2019-05-11 20:58:04 +0800

    oom

---
+++ src/main/java/com/argsno/understaingjvm/ch2/JavaMethodAreaOOM.java
@@ -0,0 +1,30 @@
+package com.argsno.understaingjvm.ch2;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
+ * Java 8 环境下已经失效
+ * @author zzm
+ */
+public class JavaMethodAreaOOM {
+    static class OOMObject {}
+
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+}
---
+++ src/main/java/com/argsno/understaingjvm/ch2/JavaVMStackOOM.java
@@ -0,0 +1,29 @@
+package com.argsno.understaingjvm.ch2;
+
+/**
+ * VM Args：-Xss2M （这时候不妨设大些）
+ * @author zzm
+ */
+public class JavaVMStackOOM {
+    private void dontStop() {
+        while (true) {
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
