ffc39c73367ab6aeae5914d44cdd35a7786cde8e
argsno <argsno@gmail.com>
2019-05-11 20:39:16 +0800

    dump

---
+++ src/main/java/com/argsno/understaingjvm/ch2/JavaVMStackSOF.java
@@ -0,0 +1,24 @@
+package com.argsno.understaingjvm.ch2;
+
+/**
+ * VM Args：-Xss256k
+ * @author zzm
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw e;
+        }
+    }
+}
